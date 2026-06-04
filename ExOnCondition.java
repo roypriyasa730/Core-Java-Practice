6f0c55e6681e44518ca1b46de7bd3afde71c8cdf
roypriyasa <roypriyasa730@gmail.com>
2026-06-04 12:09:31 +0530

    Add ExOnCondition and ExOnConditionif classes with initial implementations

---
+++ ExOnCondition.java
@@ -0,0 +1,12 @@
+public class ExOnCondition {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num < 0) {
+            System.out.println("it is +ve");
+        } else if (num < 0) {
+            System.out.println("it is -ve");
+        } else if (num == 0) {
+
+        }
+    }
+}
---
+++ ExOnConditionif.java
@@ -0,0 +1,15 @@
+public class ExOnConditionif {
+    public static void main(String[] args) {
+        int age = 30;
+        double height = 5.4;
+        double salary = 95000;
+        if (age >= 27 && age <= 30) {
+            if (height > 5.7) {
+                if (salary > 90000) {
+                }
+            }
+
+        }
+    }
+
+}
