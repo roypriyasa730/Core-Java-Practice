27cd34cd8f681dcf22863e2bdbf004102936f100
roypriyasa <roypriyasa730@gmail.com>
2026-06-05 09:35:35 +0530

    Update ExOnSwitch example to use switch case with value 2 and adjust launch configuration

--- ExOnSwitch.java
+++ ExOnSwitch.java
@@ -1,14 +1,16 @@
 public class ExOnSwitch {
     public static void main(String[] args) {
-        switch (1) {
+        switch (2) {
             case 1: {
                 System.out.println("case 1");
-                break;
+
             }
+                break;
             case 2: {
                 System.out.println("case 2");
-                break;
+
             }
+                break;
         }
 
     }
