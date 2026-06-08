29cacebe3f3ceb5d155953b7e373f9a65ef0195c
roypriyasa <roypriyasa730@gmail.com>
2026-06-08 21:45:40 +0530

    Add ExOnReverseTable.java to display reverse multiplication table for 6

---
+++ ExOnReverseTable.java
@@ -0,0 +1,8 @@
+public class ExOnReverseTable {
+    public static void main(String[] args) {
+        for (int i = 6; i >= 1; i--) {
+            System.out.println(6 + " x " + i + " = " + (6 * i));
+        }
+    }
+
+}
