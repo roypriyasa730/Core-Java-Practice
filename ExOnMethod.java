2b8cbe91252dce7b4181037a1ca17b58c07b3411
roypriyasa <roypriyasa730@gmail.com>
2026-06-09 10:58:39 +0530

    Add ExOnMethod.java with basic arithmetic operations: addition, subtraction, and multiplication

---
+++ ExOnMethod.java
@@ -0,0 +1,27 @@
+public class ExOnMethod {
+    public static void add() {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+
+    public static void sub() {
+        int a = 100;
+        int b = 20;
+        System.out.println(a - b);
+    }
+
+    public static void multi() {
+        int a = 100;
+        int b = 20;
+        System.out.println(a - b);
+    }
+
+    public static void main(String[] args) {
+        add();
+        sub();
+        multi();
+
+    }
+
+}
