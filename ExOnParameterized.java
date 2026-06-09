77f8a19fd332e27fef890bb90b9dcc7f82917c65
roypriyasa <roypriyasa730@gmail.com>
2026-06-09 23:26:40 +0530

    Add ExOnParameterized.java with a method to add two integers

---
+++ ExOnParameterized.java
@@ -0,0 +1,11 @@
+public class ExOnParameterized {
+
+    public static void add(int a, int b) {
+
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        add(10, 20);
+    }
+}
