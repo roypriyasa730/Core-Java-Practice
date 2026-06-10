dc14d67a30cb7b307b3b96f6277e9467840b79d7
roypriyasa <roypriyasa730@gmail.com>
2026-06-10 10:02:40 +0530

    Add launch configurations for ExpMethod and Exp classes

---
+++ ExpMethod.java
@@ -0,0 +1,12 @@
+public class ExpMethod {
+    public static void main(String[] args) {
+        System.out.println("Main Started");
+        greeting("priyasa");
+        System.out.println("main ended ");
+    }
+
+    public static void greeting(String name) {
+        System.out.println("hi " + name);
+
+    }
+}
