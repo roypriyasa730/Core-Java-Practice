66957f051daa76c1de3768262de3ce7346e34a97
roypriyasa <roypriyasa730@gmail.com>
2026-06-12 00:28:03 +0530

    Refactor ExOnSwitch to use try-with-resources for Scanner initialization

--- ExOnSwitch.java
+++ ExOnSwitch.java
@@ -2,42 +2,42 @@ import java.util.Scanner;
 
 public class ExOnSwitch {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String choice = sc.next();
-        switch (choice) {
-            case "Monday": {
-                System.out.println("Today is Monday");
-                break;
-            }
-            case "Tuesday": {
-                System.out.println("Today is Tuesday");
-                break;
-            }
+        try (Scanner sc = new Scanner(System.in)) {
+            String choice = sc.next();
+            switch (choice) {
+                case "Monday": {
+                    System.out.println("Today is Monday");
+                    break;
+                }
+                case "Tuesday": {
+                    System.out.println("Today is Tuesday");
+                    break;
+                }
 
-            case "Wednesday": {
-                System.out.println("Today is Wednesday");
-                break;
+                case "Wednesday": {
+                    System.out.println("Today is Wednesday");
+                    break;
+                }
+                case "Thursday": {
+                    System.out.println("Today is Thursday");
+                    break;
+                }
+                case "Friday": {
+                    System.out.println("Today is Friday");
+                    break;
+                }
+                case "Saturday": {
+                    System.out.println("Today is Saturday");
+                    break;
+                }
+                case "Sunday": {
+                    System.out.println("Today is Sunday");
+                    break;
+                }
+                default: {
+                    System.out.println("Invalid day");
+                }
             }
-            case "Thursday": {
-                System.out.println("Today is Thursday");
-                break;
-            }
-            case "Friday": {
-                System.out.println("Today is Friday");
-                break;
-            }
-            case "Saturday": {
-                System.out.println("Today is Saturday");
-                break;
-            }
-            case "Sunday": {
-                System.out.println("Today is Sunday");
-                break;
-            }
-            default: {
-                System.out.println("Invalid day");
-            }
-
         }
     }
 
