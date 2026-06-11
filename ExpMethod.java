1c4f6d3b4e331db9a4d4ec051ef81f44a6d7abb5
roypriyasa <roypriyasa730@gmail.com>
2026-06-12 00:23:59 +0530

    Add overloaded greeting method to include age parameter

--- ExpMethod.java
+++ ExpMethod.java
@@ -9,4 +9,10 @@ public class ExpMethod {
         System.out.println("hi " + name);
 
     }
+
+    public static void greeting(String name, int age) {
+        System.out.println("hi " + name + " your age is " + age);
+
+    }
+
 }
