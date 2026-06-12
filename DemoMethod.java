98f8d4d979b9992d4bd7981a9e37ffd1d1221324
roypriyasa <roypriyasa730@gmail.com>
2026-06-12 20:24:51 +0530

    Add launch configuration for DemoMethod and create DemoMethod class with main method

---
+++ DemoMethod.java
@@ -0,0 +1,14 @@
+public class DemoMethod {
+
+    static void display() {
+        System.out.println("Java");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+        display();
+        display();
+
+    }
+}
