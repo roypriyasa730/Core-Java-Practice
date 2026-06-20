8c9340f65a5c61f5d3ce10ac4b082bc4d5ebda99
roypriyasa <roypriyasa730@gmail.com>
2026-06-20 23:15:07 +0530

    Add launch configuration for ExOnShirt and update method visibility in ExOnShirt class

--- ExOnShirt.java
+++ ExOnShirt.java
@@ -4,31 +4,34 @@ public class ExOnShirt {
     String size;
     double price;
 
-    void assignShirtDetails(String b, String c, String s, double p) {
+    public void assignShirtDetails(String b, String c, String s, double p) {
         brand = b;
         color = c;
         size = s;
         price = p;
+
     }
 
-    void displayShirtDetails() {
-        System.out.println("Brand: " + brand);
-        System.out.println("Color: " + color);
-        System.out.println("Size: " + size);
-        System.out.println("Price: $" + price);
+    public void displayShirtDetails() {
+        if (price > 2000)
+            System.out.println(brand);
+        System.out.println(color);
+        System.out.println(size);
+        System.out.println(price);
     }
 
     public static void main(String[] args) {
         ExOnShirt shirt1 = new ExOnShirt();
-        shirt1.assignShirtDetails("Nike", "Red", "M", 29.99);
+        shirt1.assignShirtDetails("Nike", "Red", "M", 2400);
         shirt1.displayShirtDetails();
 
         ExOnShirt shirt2 = new ExOnShirt();
-        shirt2.assignShirtDetails("Adidas", "Blue", "L", 34.99);
+        shirt2.assignShirtDetails("Adidas", "Blue", "L", 3000);
         shirt2.displayShirtDetails();
 
         ExOnShirt shirt3 = new ExOnShirt();
-        shirt3.assignShirtDetails("Puma", "Green", "S", 24.99);
+        shirt3.assignShirtDetails("Puma", "Green", "S", 2400);
         shirt3.displayShirtDetails();
     }
+
 }
