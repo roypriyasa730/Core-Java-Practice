90dacefc54ac4272c01aefd66d8838d7a33ed14c
roypriyasa <roypriyasa730@gmail.com>
2026-06-25 12:43:03 +0530

    Add new Random class with constructor and method for display

---
+++ Random.java
@@ -0,0 +1,12 @@
+public class Random {
+    int x = 3;
+    int y = x;
+    static int p = 56;
+
+    Random() {
+        System.out.println(x + y + p);
+    }
+
+public void m1(){
+    System.out.println("hi im m1 from Random class");
+}
