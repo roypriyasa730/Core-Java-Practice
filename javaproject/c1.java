a10fc5fe1aa5138889f2308875fc403b45ff84d3
roypriyasa <roypriyasa730@gmail.com>
2026-07-05 14:22:52 +0530

    Add instance of c3 class to c1 class

--- javaproject/c1.java
+++ javaproject/c1.java
@@ -10,6 +10,8 @@ public class c1 {
         System.out.println(a++ + b);
     }
 
+    c3 obj = new c3();
+
     public void display() {
         System.out.println(a);
         System.out.println(b);
