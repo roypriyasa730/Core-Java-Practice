aff4208b056f6e5605ba980883f72b84bcf5a094
roypriyasa <roypriyasa730@gmail.com>
2026-07-12 23:22:19 +0530

    Refactor main method in Student1 class to remove duplicate initialization of s1

--- Student1.java
+++ Student1.java
@@ -15,6 +15,11 @@ public class Student1 {
         System.out.println(s1);
         System.out.println(s1.toString());
 
+        s1.name = "priya";
+        s1.id = 123;
+        s1.age = 45;
+        System.out.println(s1.toString());
+
         Student1 s2 = new Student1();
         s2.name = "gadhu";
         s2.id = 121;
