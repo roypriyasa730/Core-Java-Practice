b99e5a0fa658d1de814efdbe13e3fecb42071e24
roypriyasa <roypriyasa730@gmail.com>
2026-07-14 17:45:56 +0530

    Add launch configurations for LeapYear, Maximum, Number, and Even classes

--- C1.java
+++ C1.java
@@ -3,11 +3,6 @@ public class C1 {
     int b;
     static int k = 5;
 
-    public C1() {
-        b = 9;
-        System.out.println(a++ + b);
-    }
-
     public void display() {
         System.out.println(a);
         System.out.println(b);
