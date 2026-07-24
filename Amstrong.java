9607d845570554304cfb9d408c9bca49d35cac48
roypriyasa <roypriyasa730@gmail.com>
2026-07-24 17:37:38 +0530

    Add Amstrong class to check if a number is an Armstrong number

---
+++ Amstrong.java
@@ -0,0 +1,28 @@
+class Amstrong {
+    public static void main(String[] args) {
+        int num = 153;
+        int count = 0, temp = num;
+        int sum = 0;
+        // Count the number of digits
+        while (temp != 0) {
+            count++;
+            temp = temp / 10;
+        }
+
+        temp = num;
+        // Calculate the sum of the digits raised to the power of count
+        while (temp != 0) {
+            int fact = 1;
+            for (int i = 1; i <= count; i++) {
+                fact = fact * (temp % 10);
+            }
+            sum = sum + fact;
+            temp /= 10;
+        }
+        if (sum == num) {
+            System.out.println("amstrong");
+        } else {
+            System.out.println("not amstrong");
+        }
+    }
+}
