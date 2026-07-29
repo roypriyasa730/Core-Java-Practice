ca898cfbe254cedb520e25ba8ddf7d4a62e2e878
roypriyasa <roypriyasa730@gmail.com>
2026-07-29 23:46:05 +0530

    Add ExOnSatic class with static variables and display method

---
+++ ExOnSatic.java
@@ -0,0 +1,9 @@
+public class ExOnSatic {
+    static int a = 10;
+    static int b = 20;
+
+    static void display() {
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+}
