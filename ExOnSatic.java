72eeef23f9706661fce95b691b5748bf3e4e1565
roypriyasa <roypriyasa730@gmail.com>
2026-07-31 22:29:04 +0530

    Remove unnecessary blank lines in ExOnSatic class

--- ExOnSatic.java
+++ ExOnSatic.java
@@ -2,6 +2,8 @@ public class ExOnSatic {
     static int a = 10;
     static int b = 20;
 
+    
+
     static void display() {
         System.out.println("a: " + a);
         System.out.println("b: " + b);
@@ -9,5 +11,6 @@ public class ExOnSatic {
 
     public static void main(String[] args) {
         display();
+
     }
 }
