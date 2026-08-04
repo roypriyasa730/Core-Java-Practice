7726b66484d757bb1ba42178f22d4483a02ad4d1
roypriyasa <roypriyasa730@gmail.com>
2026-08-04 23:27:21 +0530

    Refactor AuthenticationPopup class by organizing import statements

--- AuthenticationPopup.java
+++ AuthenticationPopup.java
@@ -1,13 +1,14 @@
 
 import java.net.URI;
 import java.util.function.Predicate;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AuthenticationPopup {
 
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://the-internet.herokuapp.com/basic_auth");
 
         driver.quit();
