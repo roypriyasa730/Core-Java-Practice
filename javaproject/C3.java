7241cc6dcfae732513a6f53da65b6077942351c1
roypriyasa <roypriyasa730@gmail.com>
2026-08-06 00:10:56 +0530

    Fix class name casing for C1 in C3 main method

--- javaproject/C3.java
+++ javaproject/C3.java
@@ -3,8 +3,8 @@ package javaproject;
 public class C3 {
 
     public static void main(String[] args) {
-        C1 cc = new C1();
-        System.out.println(C1.k); // Recommended
+        c1 cc = new c1();
+        System.out.println(c1.k); // Recommended
         cc.display();
     }
 }
