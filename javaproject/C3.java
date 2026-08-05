50380026c03dfc0c14f0627f8d899046d3edb308
roypriyasa <roypriyasa730@gmail.com>
2026-08-06 00:10:19 +0530

    Add C3 class with main method to demonstrate C1 functionality

---
+++ javaproject/C3.java
@@ -0,0 +1,10 @@
+package javaproject;
+
+public class C3 {
+
+    public static void main(String[] args) {
+        C1 cc = new C1();
+        System.out.println(C1.k); // Recommended
+        cc.display();
+    }
+}
