bdf752026bd9f6ebad5cb8750bd04c042f25f337
roypriyasa <roypriyasa730@gmail.com>
2026-08-09 00:01:20 +0530

    Fix output message placement in ExOnCondition class

--- ExOnCondition.java
+++ ExOnCondition.java
@@ -3,11 +3,15 @@ public class ExOnCondition {
         int num = 5;
         if (num < 0) {
             System.out.println("it is +ve");
+
         } else if (num < 0) {
             System.out.println("it is -ve");
+            System.out.println("End of program");
         } else if (num == 0) {
-
+            System.out.println("it is zero");
+            System.out.println("End of program");
         }
         System.out.println("End of program");
+
     }
 }
