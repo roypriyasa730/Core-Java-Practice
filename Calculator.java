7462673c623fe034aa616f5c68ae203b7ea6c17f
roypriyasa <roypriyasa730@gmail.com>
2026-08-10 17:28:22 +0530

    Add Calculator class with overloaded add methods for integers and doubles

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Three integers
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Two doubles
+    double add(double a, double b) {
+        return a + b;
+    }
+}
