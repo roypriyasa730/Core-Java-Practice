6bbd22f4f5da312e12c3c7c5859a03f052ea000b
roypriyasa <roypriyasa730@gmail.com>
2026-09-06 23:56:45 +0530

    Add launch configuration for ExOnStudent and update output messages to use System.out

--- ExOnStudent.java
+++ ExOnStudent.java
@@ -7,17 +7,17 @@ public class ExOnStudent {
     }
 
     ExOnStudent(String name) {
-        System.err.println("Parameterized constructor with name: " + name);
+        System.out.println("Parameterized constructor with name: " + name);
 
     }
 
     ExOnStudent(String name, int age) {
-        System.err.println("Parameterized constructor with name: " + name + " and age: " + age);
+        System.out.println("Parameterized constructor with name: " + name + " and age: " + age);
     }
-}
 
-public static void main(String[] args) {
-    ExOnStudent student1 = new ExOnStudent();
-    ExOnStudent student2 = new ExOnStudent("John");
-    ExOnStudent student3 = new ExOnStudent("Jane", 20);
+    public static void main(String[] args) {
+        ExOnStudent s1 = new ExOnStudent();
+        ExOnStudent s2 = new ExOnStudent("John");
+        ExOnStudent s3 = new ExOnStudent("Jane", 20);
+    }
 }
